c1837dc33c7c2607950e5d74a6386a2c01eb3c4b
febrianandy <febyianandy@gmail.com>
2024-07-04 16:23:10 +0700

    cleaning

--- app/Views/partials/head.php
+++ app/Views/partials/head.php
@@ -1,16 +1,14 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/style.css" />
     <title>Hello, world!</title>
     <style>
-    /* Custom styles for login form */
+      
     .login-form {
       max-width: 350px;
       margin: 0 auto;
--- public/index.php
+++ public/index.php
@@ -4,7 +4,6 @@ use App\Core\Router;
 
 require_once __DIR__ . "/../vendor/autoload.php";
 require_once __DIR__ . "/../config/config.php";
-
 require_once __DIR__ . '/../routes/api.php';
 
 Router::run();
